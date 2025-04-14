be1c045e7aa5034cd330361d7500ddec90d1838b
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-04-14 18:03:40 -0300

    Delete - deletando produto pelo id

--- src/main/java/com/hen/aula/controllers/ProductController.java
+++ src/main/java/com/hen/aula/controllers/ProductController.java
@@ -11,8 +11,11 @@ import com.hen.aula.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,11 +32,14 @@ public class ProductController {
     // quando quiser mudar o método do service
 
     @GetMapping(value = "/{id}") //Método GET para retorna um ProductDTO pelo id
-    public ProductDTO findById(@PathVariable Long id){ // Long id é o parâmetro que vai ser o id
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){ // Long id é o parâmetro que vai ser o id
         // do produto, para colocar parâmetro na rota(ul) tem que colocar o @PathVariable
         // que vai casar com o value do GetMapping
         ProductDTO dto = service.findById(id);
-       return dto;
+       return ResponseEntity.ok(dto);// estamos dizendo que vai retornar o response
+        // entity dto, mesagem ok  http 200 que de certo,  onde o corpo vai ser o dto
+        //  dto = service.insert(dto);
+        //  return dto;
 
        // para resumir as lihas acima return service.findById(id)
         //como o service já tem o DTO n precisa colocar essa varial
@@ -44,24 +50,71 @@ public class ProductController {
     // Buscando todos os produtos páginados
     @GetMapping
     // quando é paginado tem que ser Page não list
-    public Page<ProductDTO> findAll(Pageable pageable) { // Pageable é do data.domain do spring para paginar páginas
-        return service.findAll(pageable);
+    // quando colocamos o ResponseEntity nós estamos dizendo
+    // que o método vai retornar um response entity
+    // onde corpo da resposta vai ser o dto
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) { // Pageable é do data.domain do spring para paginar páginas
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto); //
+
+       // return service.findAll(pageable);
     }
 
     //Inserindo produto na tabela
     @PostMapping
-    public ProductDTO insert (@RequestBody ProductDTO dto) {
+    // quando colocamos o ResponseEntity nós estamos dizendo
+    // que o método vai retornar um response entity
+    // onde corpo da resposta vai ser o dto
+    public ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto) {
         // Ele tem o parâmetro ProductDTO que é o objeto
         // que vai ser declarado no JSON (que esperamos receber no JSON)
         // e para que possa colocar um objeto no corpo da requisão JSON
         // tem que ter o @ RequestBody(responsável por dizer que esse parâmetro
         // ProductDo dto vai ser o corpo da requisição
         dto = service.insert(dto);
-        return dto;
+        // No cabeçalho da resposta vai ter o link para o recurso craido
+        // que é essa URI que criamos abaixo
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}") // esse path é para mostrar
+                // no Header da requisição o link ul do recurso/ produto que foi criado
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto); // estamos dizendo que vai retornar o response
+        // entity dto, mesagem created que  retorna 201 que é criação,
+        // nesse  caso tem que instanciar uma URI porque o created recebe uma URI
+
+      //  return dto;
 
         // se quiser fazer direto é return service.insert(dto)
     }
 
+    //Atualizando Produto
+    @PutMapping(value="/{id}")// a rota ul do PUT esper o id
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+        //PathVariable é para que esse parmaetro do método Long id, seja passado
+        // na url como argumento e o RequestBody é para que o produto seja passado
+        // no corpo da requisição
 
+        dto = service.update(id, dto); // atualiza o id e o produtoDto fornecido n URL
 
+        return ResponseEntity.ok(dto); // ok para retornar 200 http
+    }
+
+    //Deletando produto pelo id
+    @DeleteMapping(value = "/{id}")
+    // o Void no responseEntity quer dizer que ele n vai retornar nada
+    // só deletar
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        // PathVariable id é para indicar na ul qual produto vamos deletar
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+        // o noContent retorna o 204 que é para dizer que deu sucesso na requisição
+        // porém não retorna nada esse método, e o .build é para ele
+        // construtir certinho  o ResponseEntity
+
+ /*
+    IMPORTANTE: se vocÊ tentar apagar um produto que faz parte de um pedido(se n tabela do pedido ficaria
+    com o produto fantasma)  por padrão no JPA(banco de dados) vai dar falha de integridade referencial
+    */
+        /*    Importante: se tentar Apagar um produto de um ID inexistente o SPRING versão atual não da mais erro 500,  ele continua retornar 204 como se fosse deletado com sucesso,
+    mas nesse caso o certo é ter uma exceção*/
+    }
 }
--- src/main/java/com/hen/aula/services/ProductService.java
+++ src/main/java/com/hen/aula/services/ProductService.java
@@ -64,25 +64,81 @@ public class ProductService {
         @Transactional // como não é só leitura não pode  ter o read Onlu
         public ProductDTO insert(ProductDTO dto) { // pegar os dados do PRODUCTDTO e salvar
             // e salvar no banco de dados
-            Product product = new Product();
+            Product entity = new Product();
             //Copiando as informações do ProductDTO que é  o parâmetro do Método
             // para entidade Product
-            product.setName(dto.getName());
-            product.setDescription(dto.getDescription());
-            product.setPrice(dto.getPrice());
-            product.setImgUrl(dto.getImgUrl());
-
+            // vamos criar o método copyENtitytoDto para copiar dados de um
+            // DTO para um produto
+            copyDtoToEntity(dto, entity);
             // Salvar novo produto  no banco com as informações do DTO
-            product = repository.save(product);
-
-
+            entity = repository.save(entity);
             // retorna um DTO(para converte para DTO novamente
 
             //RESUMO:
             // Intanciamos o objeto product, copiamos os atributos do objeto ProductDTO
             // salvamos o objeto Product atualizado com as informações do DTO
             // e retornamos abaixo o objeto salvoa e atualizado
-            return new ProductDTO(product);
+            return new ProductDTO(entity);
+        }
+
+        @Transactional // como não é leitura, não tem o only rea
+        public ProductDTO update (Long id, ProductDTO dto) {
+            /*Esse método recebe 2 argumentos como parâmetro
+            *1°  é o id que vai estar na url e os detalhes do produto que vai
+            *estar no corpo da requisição*/
+
+            //Aqui  não vamos criar um novo produto com o new ProductDTO
+            // nós vamos apenas instanciar um produto com o id do produto
+            // na qualquer queremos atualizar
+
+            // Instanciando produto com a referencia do Id (prepara objeto, n cria
+            // nem consulta)
+            Product entity = repository.getReferenceById(id);//essa ação
+            // getReferenceById do repository(JPA) não vai no banco de dados
+            // ao contrário do findById da JPA repository que vai no banco de dados
+            // e busca o produto, esse getReferenceById ele só prepara o objeto
+            // monitorado pela JPA, diferente quando você faz o new Product
+            // diferente também do new ProductDto que é um produto novo
+            // que não salvamos  ainda e nem buscamos no banco de dados
+            // por isso nesse caso do new ProductDTO temos que fazer
+            // responsitory.save(product) para salvar o produto no banco de dados
+
+            // Copiando dados do DTO para o produto que foi instanciado acima com a referencia do Id
+            // obs: aqui quanto no método POST só tem que ser os atributos
+            // que existem no DTO e não todos os atributos do produto
+            // vamos criar o método copyENtitytoDto para copiar dados de um
+            // DTO para um produto
+
+            copyDtoToEntity(dto, entity);
+
+            /*entity.setName(dto.getName());
+            entity.setDescription(dto.getDescription());
+            entity.setPrice(dto.getPrice());
+            entity.setImgUrl(dto.getImgUrl());*/
+
+            // salvando produto instanciado no banco de dados
+            entity = repository.save(entity);
+            // retornando produto atualizado
+            return new ProductDTO(entity);
+        }
+
+        //Deletando produto do banco de dados pelo Id
+        @Transactional
+        // O método vai ser void porque ele só  deleta não retorna nada
+        // e vai receber um id como argumento para dizer qual produto
+        // será deletado
+        public void delete(Long id) {
+            repository.deleteById(id);// só passar o id que essa função
+            // do deleteById do Jpa deleta no banco de dado
+        }
+
+        // método void para copiar os dados do DTO para entidade produto
+        public void copyDtoToEntity(ProductDTO dto, Product entity) {
+
+            entity.setName(dto.getName());
+            entity.setDescription(dto.getDescription());
+            entity.setPrice(dto.getPrice());
+            entity.setImgUrl(dto.getImgUrl());
         }
 
 }
